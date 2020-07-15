b4771dcd5c147911bce985c1bf6be9b1f14d4e55
Mickey <detrodoniv@yahoo.com>
2020-07-14 21:51:04 -0400

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
-get "#mobs", to: "home#mobsindex"
+get "/mobs", to: "home#mobsindex"
 
-get "#monst", to: "home#monstindex"
+get "/monst", to: "home#monstindex"
 
-get "#monstrandom", to: "home#random" 
+get "/monstrandom", to: "home#random" 
 
-get "#mobs#:id", to: "home#mobshow"
+get "/mobs#:id", to: "home#mobshow"
 end
